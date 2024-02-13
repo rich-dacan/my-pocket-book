8a14a3919e61cacc59d8f68c51d9c90266d51e3c
Richard Cândido <rich_dacan@protonmail.com>
2024-02-13 14:24:03 -0300

    feat :sparkles: theme pallet

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,9 +16,48 @@ export const theme = extendTheme({
       900: "#0D47A1",
     },
     // Redefine only one shade of blue
-    blue: {
-      400: "#29B6F6",
+    yellow: {
+      400: "#FFCA28",
+      500: "#FFCF52",
+      600: "#FFC107",
+      700: "#FFB300",
     },
+    green: {
+      400: "#66BB6A",
+      500: "#4CAF50",
+      600: "#43A047",
+      700: "#388E3C",
+    },
+    red: {
+      400: "#EF5350",
+      500: "#F44336",
+      600: "#E53935",
+      700: "#D32F2F",
+    },
+    gray: {
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+    },
+    white: "#fff",
+    black: "#000",
+  },
+  fonts: {
+    // Changing default font
+    heading: "Roboto_700Bold",
+    body: "Roboto_400Regular",
+  },
+  fontSizes: {
+    // Changing default sizes
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6rem",
+    "9xl": "8rem",
   },
   config: {
     // Changing initialColorMode to 'dark'
